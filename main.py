fd4160df9c207220d5f52ba81d153c7d98e320db
2024tm05054-lab <2024tm05054@wilp.bits-pilani.ac.in>
2026-04-24 13:25:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from src.api.routes import router   
-
-app = FastAPI()
-app.include_router(router)
+app = FastAPI(
+    title="Multimodal RAG System",
+    version="0.1.0",
+    docs_url="/docs",        
+    redoc_url="/redoc",      
+    openapi_url="/openapi.json")
