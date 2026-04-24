fa389d23d1b7cb50a75a9aef4cc1d3e51276bdc4
2024tm05054-lab <2024tm05054@wilp.bits-pilani.ac.in>
2026-04-24 08:47:22 +0530

    Update vector_store.py

--- src/retrieval /vector_store.py
+++ src/retrieval /vector_store.py
@@ -1,8 +1,15 @@
 def search(self, query):
     results = []
 
+    # match based on words
     for text in self.texts:
         if any(word in text.lower() for word in query.lower().split()):
             results.append(text)
 
-    return results[:3]
+    # ensure table + image always included
+    for text in self.texts:
+        if "Table:" in text or "Image:" in text:
+            if text not in results:
+                results.append(text)
+
+    return results[:5]
